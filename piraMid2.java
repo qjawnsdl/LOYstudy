d5260bf41474d7530ea32baa14c137e12cc54d14
qjawnsdl <165367880+qjawnsdl@users.noreply.github.com>
2024-04-13 03:48:09 +0900

    Add files via upload

--- piraMid2.java
+++ piraMid2.java
@@ -1,13 +1,15 @@
 public class piraMid2 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = i - 1; j < 10; j++) {
-                System.out.print("*");
 
-                    System.out.println("*");
-                
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10 - i - 1; j++) {
+                System.out.print(" ");
             }
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
